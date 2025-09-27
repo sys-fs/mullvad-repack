6ceeac4a37516e4b49c89313efb8b34e89139621
Thomas Mannay <tfm@disroot.org>
2025-09-28 00:36:29 +0100

    Add runit support, tidy up

--- repack.sh
+++ repack.sh
@@ -8,11 +8,12 @@ fi
 set -u
 
 version="$1"
+init_system="sysvinit"
 
 if [ -x /sbin/openrc-run ]; then
-	cd openrc
-else
-	cd sysvinit
+	init_system="openrc"
+elif [ -x /usr/bin/sv ]; then
+	init_system="runit"
 fi
 
 rm -rf repack
@@ -25,14 +26,21 @@ dpkg-deb -e "MullvadVPN-${version}_amd64.deb" repack/DEBIAN
 # The only things in here are systemd units.
 rm -rf repack/usr/lib
 
-# Add OpenRC init scripts.
-mkdir -p repack/etc/init.d
-cp mullvad-daemon repack/etc/init.d
-cp mullvad-early-boot-blocking repack/etc/init.d
+# Add init scripts.
+if [ $init_system = "runit" ]; then
+	mkdir -p repack/etc/sv
+	cp -r "${init_system}/mullvad-daemon" repack/etc/sv
+else
+	mkdir -p repack/etc/init.d
+	cp "${init_system}/mullvad-daemon" repack/etc/init.d
+fi
+
+mkdir -p repack/etc/cron.d
+cp mullvad-early-boot-blocking repack/etc/cron.d
 
 # Patch the packaging scripts.
-patch -l repack/DEBIAN/preinst < preinst.patch
-patch -l repack/DEBIAN/postinst < postinst.patch
-patch -l repack/DEBIAN/prerm < prerm.patch
+patch -l repack/DEBIAN/preinst < "${init_system}/preinst.patch"
+patch -l repack/DEBIAN/postinst < "${init_system}/postinst.patch"
+patch -l repack/DEBIAN/prerm < "${init_system}/prerm.patch"
 
-dpkg-deb -b repack "../mullvad-${version}-repack.deb"
+dpkg-deb -b repack "mullvad-${version}-repack.deb"
