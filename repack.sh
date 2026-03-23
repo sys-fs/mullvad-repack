1534d0f95da998bd7359ea1084eeb16315e5cef6
Thomas Mannay <tfm@disroot.org>
2026-03-23 02:45:04 +0000

    Consolidate patches into one set

--- repack.sh
+++ repack.sh
@@ -2,24 +2,12 @@
 set -eo pipefail
 
 if [ -z "$1" ]; then
-    echo "usage: repack.sh version [init-system]"
+    echo "usage: repack.sh version"
     exit 1
 fi
 set -u
 
 version="$1"
-
-if [ -n "$2" ]; then
-    init_system="$2"
-else
-    init_system="sysvinit"
-
-    if [ -x /sbin/openrc-run ]; then
-	init_system="openrc"
-    elif [ -x /usr/bin/sv ]; then
-	init_system="runit"
-    fi
-fi
 
 rm -rf repack
 
@@ -40,21 +28,15 @@ dpkg-deb -e "$file" repack/DEBIAN
 rm -rf repack/usr/lib
 
 # Add init scripts.
-if [ $init_system = "runit" ]; then
-    mkdir -p repack/etc/sv
-    cp -r "runit/mullvad-daemon" repack/etc/sv
-else
-    mkdir -p repack/etc/init.d
-    cp "${init_system}/mullvad-daemon" repack/etc/init.d
-fi
-
-mkdir -p repack/etc/cron.d
+mkdir -p repack/etc/{cron.d,init.d,sv}
+cp -r mullvad-daemon-runit repack/etc/sv/mullvad-daemon
+cp mullvad-daemon repack/etc/init.d
 cp mullvad-early-boot-blocking repack/etc/cron.d
 chmod go-w repack/etc/cron.d/mullvad-early-boot-blocking
 
 # Patch the packaging scripts.
-patch -l repack/DEBIAN/preinst < "${init_system}/preinst.patch"
-patch -l repack/DEBIAN/postinst < "${init_system}/postinst.patch"
-patch -l repack/DEBIAN/prerm < "${init_system}/prerm.patch"
+patch -l repack/DEBIAN/preinst preinst.patch
+patch -l repack/DEBIAN/postinst postinst.patch
+patch -l repack/DEBIAN/prerm prerm.patch
 
-dpkg-deb --root-owner-group --build repack "${file%".deb"}-${init_system}.deb"
+dpkg-deb --root-owner-group --build repack "${file%".deb"}.deb"
