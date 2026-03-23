cb07ebaa5cd00f631629426342721360a52bb478
Thomas Mannay <tfm@disroot.org>
2026-03-23 00:46:00 +0000

    Test package building in GitHub Actians

--- repack.sh
+++ repack.sh
@@ -2,28 +2,35 @@
 set -eo pipefail
 
 if [ -z "$1" ]; then
-	echo "usage: repack.sh version"
-	exit 1
+    echo "usage: repack.sh version [init-system]"
+    exit 1
 fi
 set -u
 
 version="$1"
-init_system="sysvinit"
 
-if [ -x /sbin/openrc-run ]; then
+if [ -n "$2" ]; then
+    init_system="$2"
+else
+    init_system="sysvinit"
+
+    if [ -x /sbin/openrc-run ]; then
 	init_system="openrc"
-elif [ -x /usr/bin/sv ]; then
+    elif [ -x /usr/bin/sv ]; then
 	init_system="runit"
+    fi
 fi
 
 rm -rf repack
 
 case "$version" in
-	*".deb")
-		file="$version";;
-	*)
-		file="MullvadVPN-${version}_$(dpkg --print-architecture).deb"
-		wget -nc "https://github.com/mullvad/mullvadvpn-app/releases/download/${version}/${file}";;
+    *".deb")
+	file="$version"
+	;;
+    *)
+	file="MullvadVPN-${version}_$(dpkg --print-architecture).deb"
+	wget -nc "https://github.com/mullvad/mullvadvpn-app/releases/download/${version}/${file}"
+	;;
 esac
 
 dpkg-deb -x "$file" repack
@@ -34,11 +41,11 @@ rm -rf repack/usr/lib
 
 # Add init scripts.
 if [ $init_system = "runit" ]; then
-	mkdir -p repack/etc/sv
-	cp -r "${init_system}/mullvad-daemon" repack/etc/sv
+    mkdir -p repack/etc/sv
+    cp -r "runit/mullvad-daemon" repack/etc/sv
 else
-	mkdir -p repack/etc/init.d
-	cp "${init_system}/mullvad-daemon" repack/etc/init.d
+    mkdir -p repack/etc/init.d
+    cp "${init_system}/mullvad-daemon" repack/etc/init.d
 fi
 
 mkdir -p repack/etc/cron.d
@@ -50,4 +57,4 @@ patch -l repack/DEBIAN/preinst < "${init_system}/preinst.patch"
 patch -l repack/DEBIAN/postinst < "${init_system}/postinst.patch"
 patch -l repack/DEBIAN/prerm < "${init_system}/prerm.patch"
 
-dpkg-deb --root-owner-group -b repack "${file%".deb"}-repack.deb"
+dpkg-deb --root-owner-group --build repack "${file%".deb"}-${init_system}.deb"
