683c65d5867533b29c547b7957bbab7c4932ed4f
Thomas Mannay <tfm@disroot.org>
2026-03-23 11:32:58 +0000

    Fix #7: Early boot network blocking issues on non-runit hosts. (#8)

--- repack.sh
+++ repack.sh
@@ -2,28 +2,30 @@
 set -eo pipefail
 
 if [ -z "$1" ]; then
-	echo "usage: repack.sh version"
-	exit 1
+    echo "usage: repack.sh version [arch]"
+    exit 1
 fi
-set -u
 
 version="$1"
-init_system="sysvinit"
 
-if [ -x /sbin/openrc-run ]; then
-	init_system="openrc"
-elif [ -x /usr/bin/sv ]; then
-	init_system="runit"
+if [ -z "$2" ]; then
+    arch=$(dpkg --print-architecture)
+else
+    arch="$2"
 fi
+
+set -u
 
 rm -rf repack
 
 case "$version" in
-	*".deb")
-		file="$version";;
-	*)
-		file="MullvadVPN-${version}_$(dpkg --print-architecture).deb"
-		wget -nc "https://github.com/mullvad/mullvadvpn-app/releases/download/${version}/${file}";;
+    *".deb")
+	file="$version"
+	;;
+    *)
+	file="MullvadVPN-${version}_${arch}.deb"
+	wget -nc "https://github.com/mullvad/mullvadvpn-app/releases/download/${version}/${file}"
+	;;
 esac
 
 dpkg-deb -x "$file" repack
@@ -33,21 +35,14 @@ dpkg-deb -e "$file" repack/DEBIAN
 rm -rf repack/usr/lib
 
 # Add init scripts.
-if [ $init_system = "runit" ]; then
-	mkdir -p repack/etc/sv
-	cp -r "${init_system}/mullvad-daemon" repack/etc/sv
-else
-	mkdir -p repack/etc/init.d
-	cp "${init_system}/mullvad-daemon" repack/etc/init.d
-fi
-
-mkdir -p repack/etc/cron.d
-cp mullvad-early-boot-blocking repack/etc/cron.d
-chmod go-w repack/etc/cron.d/mullvad-early-boot-blocking
+mkdir -p repack/etc/{init.d,sv}
+cp -r runit/mullvad-daemon repack/etc/sv/mullvad-daemon
+cp sysv/mullvad-daemon repack/etc/init.d
+cp sysv/mullvad-early-boot-blocking repack/etc/init.d
 
 # Patch the packaging scripts.
-patch -l repack/DEBIAN/preinst < "${init_system}/preinst.patch"
-patch -l repack/DEBIAN/postinst < "${init_system}/postinst.patch"
-patch -l repack/DEBIAN/prerm < "${init_system}/prerm.patch"
+patch -l repack/DEBIAN/preinst preinst.patch
+patch -l repack/DEBIAN/postinst postinst.patch
+patch -l repack/DEBIAN/prerm prerm.patch
 
-dpkg-deb --root-owner-group -b repack "${file%".deb"}-repack.deb"
+dpkg-deb --root-owner-group --build repack "${file%".deb"}_repack.deb"
